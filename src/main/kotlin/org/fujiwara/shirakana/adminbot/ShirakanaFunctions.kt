de4ad8b465864adfd958f992a9566e40c6df7319
藤原白叶 <40709280+FujiwaraShirakana@users.noreply.github.com>
2022-05-25 16:29:46 +0800

    Update ShirakanaFunctions.kt

--- src/main/kotlin/org/fujiwara/shirakana/adminbot/ShirakanaFunctions.kt
+++ src/main/kotlin/org/fujiwara/shirakana/adminbot/ShirakanaFunctions.kt
@@ -1,13 +1,37 @@
 package org.fujiwara.shirakana.adminbot
 
 import net.mamoe.mirai.contact.isAdministrator
-import net.mamoe.mirai.event.*
+import net.mamoe.mirai.contact.isOwner
+import net.mamoe.mirai.event.EventHandler
+import net.mamoe.mirai.event.SimpleListenerHost
 import net.mamoe.mirai.event.events.*
-import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.*
 import net.mamoe.mirai.utils.info
-import org.fujiwara.shirakana.adminbot.configAndData.*
+import org.fujiwara.shirakana.adminbot.command.ShirakanaParanoia
+import org.fujiwara.shirakana.adminbot.configAndData.ShirakanaBigCleanSetting
+import org.fujiwara.shirakana.adminbot.configAndData.ShirakanaDataFlags
+import org.fujiwara.shirakana.adminbot.configAndData.ShirakanaDataGroupMember
+
 
 public object ShirakanaEventListener : SimpleListenerHost() {
+    /*@EventHandler
+    internal suspend fun BotOnlineEvent.handle1() {
+        val t1 = Timer()
+        val startCalendar = Calendar.getInstance()
+        startCalendar[Calendar.HOUR_OF_DAY] = 1 // 控制时
+        startCalendar[Calendar.MINUTE] = 0 // 控制分
+        startCalendar[Calendar.SECOND] = 0 // 控制秒
+        val startTime = startCalendar.time
+        t1.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                println("管理偏执度增加了2")
+                ShirakanaDataGroupMember.bigCleanParanoia += 2
+                println("当前管理偏执度："+ShirakanaDataGroupMember.bigCleanParanoia.toString())
+            }
+        }, startTime, 1000*24*60*60)
+        println("定时任务启动完毕")
+    }
+     */
     @EventHandler
     internal suspend fun MemberJoinRequestEvent.handle() {
         if(ShirakanaBigCleanSetting.repeat_join_clean){
@@ -43,9 +67,20 @@ public object ShirakanaEventListener : SimpleListenerHost() {
                 for(memberId in ShirakanaDataGroupMember.smallCleanTarget){
                     if(message.contentToString()== At(memberId.toLong()).contentToString()){
                         val member = group.get(memberId.toLong())
-                        group.sendMessage("群友："+member?.nick+"已被清洗\n记得输入“结束清洗”来结束大清洗")
-                        member?.kick("你已被清洗")
-                        ShirakanaDataGroupMember.ShirakanaBlackListGroup.add(memberId)
+                        if(member!=null){
+                            if(member.isAdministrator()||member.isOwner()){
+                                group.sendMessage("你无权清洗管理员")
+                                continue
+                            }
+                            val msgChain = buildMessageChain {
+                                +PlainText("群友："+member.nick+"已被清洗\n")
+                                +ShirakanaParanoia.GetImageTutu(member,group)
+                                +PlainText("清洗结束后，记得输入“结束清洗”来结束大清洗")
+                            }
+                            group.sendMessage(msgChain)
+                            member.kick("你已被清洗")
+                            ShirakanaDataGroupMember.ShirakanaBlackListGroup.add(memberId)
+                        }
                     }
                 }
             }
@@ -71,9 +106,9 @@ public object ShirakanaEventListener : SimpleListenerHost() {
     }
     @EventHandler
     internal suspend fun BotOnlineEvent.handle(){
+        ShirakanaDataGroupMember.selectedGroupMembers.clear()
         for(group_id in ShirakanaDataGroupMember.selectedGroups){
             val selectedGroup = bot.getGroup(group_id.toLong())?.members
-            ShirakanaDataGroupMember.selectedGroupMembers.clear()
             if (selectedGroup != null) {
                 for(member in selectedGroup){
                     ShirakanaDataGroupMember.selectedGroupMembers.add(member.id.toString())
@@ -82,6 +117,18 @@ public object ShirakanaEventListener : SimpleListenerHost() {
         }
         ShirakanaAdminBot.logger.info { "群员信息重载完毕" }
     }
+    /*@EventHandler
+    internal suspend fun GroupMessageEvent.handle1() {
+        if(ShirakanaBigCleanSetting.big_clean_switch&&ShirakanaDataGroupMember.bigCleanParanoia>=100L){
+            ShirakanaDataGroupMember.bigCleanParanoia = 0
+            val tmpThisGroup = bot.getGroup(ShirakanaAdministratorList.adminGroup)
+            tmpThisGroup.sendMessage()
+        }
+        if(ShirakanaBigCleanSetting.small_clean_switch&&ShirakanaDataGroupMember.bigCleanParanoia==50L){
+
+        }
+    }
+     */
 }
 
 
