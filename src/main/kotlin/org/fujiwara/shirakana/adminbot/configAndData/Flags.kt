2bebf3172312d6efbfb0a92fc1e841d74d11b8e6
藤原白叶 <40709280+FujiwaraShirakana@users.noreply.github.com>
2022-05-25 16:27:56 +0800

    Update Flags.kt

--- src/main/kotlin/org/fujiwara/shirakana/adminbot/configAndData/Flags.kt
+++ src/main/kotlin/org/fujiwara/shirakana/adminbot/configAndData/Flags.kt
@@ -5,12 +5,12 @@ import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.ValueName
 import net.mamoe.mirai.console.data.value
 
-public object ShirakanaDataFlags : AutoSavePluginData("Shirakana_AdminBot_GroupMember_Data") {
+public object ShirakanaDataFlags : AutoSavePluginData("Shirakana_AdminBot_Flags_Data") {
     @ValueName("flagSmallCleanStart")
     @ValueDescription("小清洗状态")
     var flagSmallCleanStart: Boolean by value(false)
 
-    @ValueName("flagSmallCleanStart")
+    @ValueName("flagSmallCleanTarget")
     @ValueDescription("小清洗目标群")
     var flagSmallCleanTarget: Long by value(0L)
 }
