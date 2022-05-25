1b655a93c9ad44af901f2b3690947d6183e568ba
藤原白叶 <40709280+FujiwaraShirakana@users.noreply.github.com>
2022-05-25 16:26:55 +0800

    Update BigCleanConfig.kt

--- src/main/kotlin/org/fujiwara/shirakana/adminbot/configAndData/BigCleanConfig.kt
+++ src/main/kotlin/org/fujiwara/shirakana/adminbot/configAndData/BigCleanConfig.kt
@@ -18,7 +18,15 @@ object ShirakanaBigCleanSetting : ReadOnlyPluginConfig("Shirakana_AdminBot_Setti
     @ValueDescription("重复加群自动拒绝及清理开关")
     val repeat_join_clean: Boolean by value(true)
 
+    @ValueName("repeat_join_clean_mode")
+    @ValueDescription("重复加群白名单/黑名单（True为黑名单模式，False为白名单模式）")
+    val repeat_join_clean_mode: Boolean by value(true)
+
     @ValueName("Standard_Name")
-    @ValueDescription("重复加群自动拒绝及清理开关")
-    val nameStandard: String by value("")
+    @ValueDescription("命名标准（群备注包含此字符串的不会被大清洗）")
+    val nameStandard: String by value("（无）")
+
+    @ValueName("Standard_Time")
+    @ValueDescription("大清洗时间，最后一次发言时间在这个数值（天）之前的群友会被清洗")
+    val bigCleanTargetTimeDay: String by value("")
 }
